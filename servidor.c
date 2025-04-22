08428f990531cdfa4e4188c0bfd34d0050f2f5c8
AgusMessana <agustin200369@gmail.com>
2025-04-21 23:53:28 -0300

    Corrección al servidor

--- servidor.c
+++ servidor.c
@@ -27,93 +27,58 @@
 #define MAX_EVENTS 10
 
 pthread_mutex_t mutex_v = PTHREAD_MUTEX_INITIALIZER;
-
 int fde;
-
 int lsock;
-/*
-struct epoll_event{
-    uint32_t events; //moniteramo epollin
-    epoll_data_t data;
-} ev;
+int* memptr;
+int fd;
 
-struct epoll_data_t{
-    union epoll_data{
-        void *ptr;
-        int fd;
-        uint32_t u32;
-        uint64_t u64;
-    } data;
-};*/
-
-/*
- * Para probar, usar netcat. Ej:
- *
- *      $ nc localhost 4040
- *      NUEVO
- *      0
- *      NUEVO
- *      1
- *      CHAU
- */
-
-void quit(char *s)
-{
+void quit(char *s) {
 	perror(s);
 	abort();
 }
 
+int fd_readline(int fd, char *buf) {
+	int rc, i = 0;
 
-
-int* memptr;
-int U = 0;
-int fd;
-
-int fd_readline(int fd, char *buf)
-{
-	int rc;
-	int i = 0;
-
-	/*
-	 * Leemos de a un caracter (no muy eficiente...) hasta
-	 * completar una línea.
-	 */
-	while ((rc = read(fd, buf + i, 1)) > 0) {
-		if (buf[i] == '\n')
+	while((rc = read(fd, buf + i, 1)) > 0) {
+		if(buf[i] == '\n') {
 			break;
+		}
 		i++;
 	}
 
-	if (rc < 0)
+	if (rc < 0) {
 		return rc;
+	}
 
 	buf[i] = 0;
 	return i;
 }
 
-void handle_conn(int csock)
-{
+void handle_conn(int csock) {
 	char buf[200];
 	int rc;
 
 	while (1) {
 		/* Atendemos pedidos, uno por linea */
 		rc = fd_readline(csock, buf);
-		if (rc < 0)
+		if(rc < 0) {
 			quit("read... raro");
+		}
 
-		if (rc == 0) {
+		if(rc == 0) {
 			/* linea vacia, se cerró la conexión */
 			close(csock);
 			return;
 		}
 
-		if (!strcmp(buf, "NUEVO")) {
+		if(!strcmp(buf, "NUEVO")) {
 			char reply[20];
-			U = *memptr;
+			pthread_mutex_lock(&mutex_v);
+			int U = *memptr;
 			sprintf(reply, "%d\n", U);
-			U++;
-			*memptr = U;
+			*memptr = U + 1;
+			pthread_mutex_unlock(&mutex_v);
 			write(csock, reply, strlen(reply));
 		} else if (!strcmp(buf, "CHAU")) {
 			write(csock, "Chau!, cierro la conexión\n", 26);
@@ -123,141 +88,120 @@ void handle_conn(int csock)
 	}
 }
 
-void wait_for_clients(pthread_t lsock)
-{
-	int csock;
-	struct epoll_event ev,  events[MAX_EVENTS];
-
-    int nfds= epoll_wait(fde,events,MAX_EVENTS,-1);
-    if(nfds==-1){
-        perror("epoll_wait");
-        exit(EXIT_FAILURE);
-    }
-   // int n=MAX_EVENTS;
-    for(int n=0;n<nfds;++n){
-        if(events[n].data.fd==fd){
-            csock = accept(fd, NULL, NULL);
-            if (csock < 0)
-                quit("accept");
-                exit(EXIT_FAILURE);
-            }
-
-         //   setnonblocking(csock);
-            ev.events = EPOLLIN | EPOLLONESHOT;
-            ev.data.fd = csock;
-            if (epoll_ctl(fde, EPOLL_CTL_ADD, csock,&ev) == -1) {
-                perror("epoll_ctl: conn_sock");
-                exit(EXIT_FAILURE);
-            }else{
-               
-                handle_conn(events[n].data.fd);
-                ev.events = EPOLLIN | EPOLLONESHOT;
-                ev.data.fd =events[n].data.fd;
-                if (epoll_ctl(fde, EPOLL_CTL_MOD, events[n].data.fd,&ev) == -1) {
-                    perror("epoll_ctl: conn_sock");
-                    exit(EXIT_FAILURE);
-            }}
-        }
+void* wait_for_clients(void* arg) {
+	struct epoll_event ev, events[MAX_EVENTS];
 	
+	while (1) {
+		int nfds = epoll_wait(fde, events, MAX_EVENTS, -1);
+		if (nfds == -1) {
+			perror("epoll_wait");
+			exit(EXIT_FAILURE);
+		}
 
+		for (int n = 0; n < nfds; ++n) {
+			if (events[n].data.fd == lsock) {
+				int csock = accept(lsock, NULL, NULL);
+				if (csock < 0) {
+					quit("accept");
+					exit(EXIT_FAILURE);
+				}
+				ev.events = EPOLLIN | EPOLLONESHOT;
+				ev.data.fd = csock;
+				if (epoll_ctl(fde, EPOLL_CTL_ADD, csock, &ev) == -1) {
+					perror("epoll_ctl: conn_sock");
+					exit(EXIT_FAILURE);
+				}
+			} else {
+				handle_conn(events[n].data.fd);
+				ev.events = EPOLLIN | EPOLLONESHOT;
+				ev.data.fd = events[n].data.fd;
+				if (epoll_ctl(fde, EPOLL_CTL_MOD, events[n].data.fd, &ev) == -1) {
+					perror("epoll_ctl: rearm");
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+	}
+	return NULL;
 }
 
-/* Crea un socket de escucha en puerto 4040 TCP */
-int mk_lsock()
-{
+int mk_lsock() {
 	struct sockaddr_in sa;
-	int lsock;
-	int rc;
-	int yes = 1;
+	int sockfd, rc, yes = 1;
 
-	/* Crear socket */
-	lsock = socket(AF_INET, SOCK_STREAM, 0);
-	if (lsock < 0)
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
 		quit("socket");
-
-	/* Setear opción reuseaddr... normalmente no es necesario */
-	if (setsockopt(lsock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == 1)
+	}
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) { 
 		quit("setsockopt");
+	}
 
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(4040);
 	sa.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	/* Bindear al puerto 4040 TCP, en todas las direcciones disponibles */
-	rc = bind(lsock, (struct sockaddr *)&sa, sizeof sa);
-	if (rc < 0)
+	rc = bind(sockfd, (struct sockaddr *)&sa, sizeof sa);
+	if (rc < 0) {
 		quit("bind");
+	}
 
-	/* Setear en modo escucha */
-	rc = listen(lsock, 10);
-	if (rc < 0)
+	rc = listen(sockfd, 10);
+	if (rc < 0) {
 		quit("listen");
+	}
 
-	return lsock;
+	return sockfd;
 }
 
-/*Crea un segmento de memoria compartida*/
 void prepare_men(){
-	fd = shm_open(BACKING_FILE,      
-                    O_RDWR | O_CREAT, /* read/write, crear si es necesario */
-                    ACCESS_PERMS);     /* permisos (0644) */
-	if (fd < 0) quit("No pude abrir el segmento compartido...");
+	fd = shm_open(BACKING_FILE, O_RDWR | O_CREAT, ACCESS_PERMS);
+	if(fd < 0) {
+		quit("No pude abrir el segmento compartido...");
+	}
 
-  	ftruncate(fd, BYTE_SIZE); /* get the bytes */
+  	ftruncate(fd, BYTE_SIZE);
+	memptr = mmap(0, BYTE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if(memptr == MAP_FAILED) {
+		quit("mmap");
+	}
 
-	memptr = (int *)mmap(0,      
-                        BYTE_SIZE,   /* cuantos bytes */
-                        PROT_READ | PROT_WRITE, 
-                        MAP_SHARED, 
-                        fd,        
-                        0);         /* offset: empieza en el primer byte */
-	if ((int *) -1  == memptr) quit("No pude tener el segmento...");
-	U = *memptr;
-	*memptr = 0; /*Inicializo a 0*/
+	*memptr = 0;
 
 	fprintf(stderr, "Dirección de memoria compartida: %p [0..%d]\n", memptr, BYTE_SIZE - 1);
-	fprintf(stderr, "backing file:       /dev/shm%s\n", BACKING_FILE );
-	return;
-
+	fprintf(stderr, "backing file: /dev/shm%s\n", BACKING_FILE);
 }
 
-int main()
-
-
-{ 
-     fde=epoll_create1(0);
-    if (fde == -1) {
-        perror("epoll_create1");
+int main() { 
+    fde = epoll_create1(0);
+    if(fde == -1) {
+		perror("epoll_create1");
         exit(EXIT_FAILURE);
     }
-
- struct  epoll_event ev;
-
-
-    
-
- 
-
+	
+	struct  epoll_event ev;
 	lsock = mk_lsock();
-    
+
     ev.events = EPOLLIN;
-    ev.data.fd = mk_lsock;
-    if (epoll_ctl(fde, EPOLL_CTL_ADD, &mk_lsock, &ev) == -1) {
+    ev.data.fd = lsock;
+    if (epoll_ctl(fde, EPOLL_CTL_ADD, lsock, &ev) == -1) {
         perror("epoll_ctl: mk_lsock");
         exit(EXIT_FAILURE);
     }
     
-    
-
 	prepare_men();
-
-    for(int i=0;i<MAX_EVENTS;i++){
-        pthread_t pthread;
-        pthread_create(&pthread,NULL,(void *)wait_for_clients,&fde);
+	pthread_t threads[MAX_EVENTS];
+    for(int i = 0; i < MAX_EVENTS; i++) {
+        pthread_create(&threads[i], NULL, wait_for_clients, NULL);
     }
 
-	//munmap(memptr, BYTE_SIZE);
+	for(int i = 0; i < MAX_EVENTS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
   	close(fd);
- //	unlink(BACKING_FILE);
+	close(lsock);
+	close(fde);
+	
 	return 0;	
 }
