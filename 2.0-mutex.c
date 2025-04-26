fe584acf7a5afd73267d824ea7c924e93530a3b0
AgusMessana <agustin200369@gmail.com>
2025-04-26 14:22:06 -0300

    Elimino archivos innecesarios

--- 2.0-mutex.c
+++
@@ -1,4 +0,0 @@
-//region critica  2 quieren acceder(REGION CRTICA) - lo manejaremos con mutex (resolvemos el problema d race condition)
-
-
-
--- Ejercicios/p2.2.c
+++
@@ -1,9 +0,0 @@
-// j. 2. ¿C´uales de los siguientes componentes del estado de un programa se comparten entre los threads
-// de un proceso?
-// a) Valores de registros
-// b) Heap
-// c) Variables globales
-// d) Stack
-
-
-//TODO MENOS EL STACK + valores registro
