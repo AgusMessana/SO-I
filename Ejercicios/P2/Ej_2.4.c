c716e63d3206f4e52eaed3ef37b976e5c855ccc9
AgusMessana <agustin200369@gmail.com>
2026-04-14 23:40:48 -0300

    Ejercicios práctica 2

---
+++ Ejercicios/P2/Ej_2.4.c
@@ -0,0 +1,48 @@
+/**
+ * ¿Puede fallar la siguiente aserción?
+ * (wr y rd corren en un thread cada uno)
+ * Explique. Si puede fallar, arregle el programa.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
+
+volatile int x = 0;
+volatile int y = 0;
+
+void * wr(void *arg) { 
+  x = 123;
+  asm("mfence");
+  y = 1; 
+}
+
+void * rd(void *arg) {
+  while (!y)
+    ;
+  printf("x = %d.\n", x);
+  assert(x == 123);
+}
+
+int main(void) {
+  pthread_t h1, h2;
+  pthread_create(&h1, NULL, wr, NULL);
+  pthread_create(&h2, NULL, rd, NULL);
+
+  pthread_join(h1, NULL);
+  pthread_join(h1, NULL);
+
+  return 0;
+}
+
+/**
+ * El programa puede fallar.
+ * Aunque volatile le indica al compilador que no optimice el
+ acceso a las variables x e y, no se garantiza la atomicidad ni
+ orden de escritura. Podría pasar que wr escriba x = 123 e y = 1
+ pero el procesador podría reordenar las escrituras, provocando
+ un assert(0).
+ * Para solucionarlo usamos una barrera de memoria.
+ */
---
+++ Ejercicios/P2/Ej_2.5.c
@@ -0,0 +1,60 @@
+/**
+ * Implemente el algoritmo de Peterson para solucionar el problema del jardín ornamental. Tenga en cuenta lo discutido sobre barreras de memoria.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define CANT_VISITANTES 1000000
+int visitantes = 0;
+int flag[] = {0, 0}; // flag[0] = 1 indica que molinete0 quiere entrar, flag[1] = 1 indica que molinete1 quiere entrar.
+int turn = 0; // turn = 0 indica que molinete0 debe entrar, turn = 1 indica que molinete1 debe entrar.
+
+void *molinete0(void* arg) {
+  for(int i = 0; i < CANT_VISITANTES / 2; i++) {
+    //lock
+    flag[0] = 1;
+    turn = 1;
+    asm("mfence");
+    while(flag[1] && turn == 1);
+
+    //sección crítica
+    visitantes++;
+    printf("m0: visitantes hasta ahora: %d.\n", visitantes);
+
+    //unlock
+    flag[0] = 0;
+  }
+
+  return NULL;
+}
+
+void *molinete1(void* arg) {
+  for(int i = 0; i < CANT_VISITANTES / 2; i++) {
+    //lock
+    flag[1] = 1;
+    turn = 0;
+    asm("mfence");
+    while(flag[0] && turn == 0);
+
+    //sección crítica
+    visitantes++;
+    printf("m1: visitantes hasta ahora: %d.\n", visitantes);
+    //unlock
+    flag[1] = 0;
+  }
+
+  return NULL;
+}
+
+int main(void) {
+  pthread_t h0, h1;
+  pthread_create(&h0, NULL, molinete0, NULL);
+  pthread_create(&h1, NULL, molinete1, NULL);
+
+  pthread_join(h0, NULL);
+  pthread_join(h1, NULL);
+
+  printf("Cantidad visitantes: %d.\n", visitantes);
+  return 0;
+}
